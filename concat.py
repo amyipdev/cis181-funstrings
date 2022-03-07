89728a5b5ea7e42805884545db9e4fead6e4bee2
Amy Parker <apark0006@student.cerritos.edu>
2022-03-06 17:47:18 -0800

    fixed concat

--- concat.py
+++ concat.py
@@ -7,5 +7,5 @@ def concat(render_screen):
     s2 = render_screen("Now, enter another...")
     render_screen(f"Tada!\n\nFirst string: {s1}\n\n"\
                   f"Second string: {s2}\n\n"\
-                  f"And together: {s1.join(s2)}\n\n"
+                  f"And together: {s1 + s2}\n\n"
                   f"Press enter/return to go to the main menu.")
